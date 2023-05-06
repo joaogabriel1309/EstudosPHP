2172421e7b374b5b8b64d57f0eadb6be21dd93f5
João Gabriel de Matos Silva <gabriel.122.gm85@gmail.com>
2023-05-06 03:57:07 -0400

    desafio da tabuada

---
+++ Desafios/D005/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Divisao</title>
+</head>
+
+<body>
+    <main>
+        <h1>Anatomia de uma Divisao</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="valor1">Dividendo</label>
+            
+
+
+        </form>
+    </main>
+</body>
+
+</html>
